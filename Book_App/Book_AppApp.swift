c8d61b32706227c711e5d7b9ac4db1861cf44d89
Abhijit Mahanta <abhijit.mahanta.pm@gmail.com>
2026-06-04 08:36:50 +0530

    Initial Commit

---
+++ Book_App/Book_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_AppApp.swift
+//  Book_App
+//
+//  Created by Abhijit Mahanta on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Book_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
